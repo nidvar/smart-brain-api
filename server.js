8ae648e999cc413ad27ee2abf6b40db0883af724
Nidvar <djayshine87@hotmail.com>
2021-04-28 17:25:04 +1000

    connection string

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const profile_stuff = require('./controllers/profile')
 const database = knex({
     client: 'pg',
     connection: {
-      host : process.env.DATABASE_URL,
+      connectionString : process.env.DATABASE_URL,
       ssl: true,
     }
 });
